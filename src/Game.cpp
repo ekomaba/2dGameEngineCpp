808e003a0a86b5bf734365a2e716f074ea96f432
Eduardo Komaba <ekomaba@proton.me>
2025-01-14 20:38:59 -0300

    Setup SDL initialization

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 #include "Game.h"
 
 Game::Game() {
@@ -10,6 +11,34 @@ Game::~Game() {
 }
 
 void Game::Initialize() {
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+
+    // Creating window
+    SDL_Window* window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS)
+        ;
+
+    if (!window) {
+        std::cerr << "Error creating SDL Window." << std::endl;
+        return;
+    }
+    
+    // Creating Renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if (!renderer) {
+        std::cerr << "Error creating SDL Renderer window." << std::endl;
+        return;
+    }
 
 }
 
