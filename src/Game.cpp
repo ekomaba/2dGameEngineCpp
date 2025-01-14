f994005a70161ec68e2934e22a9d698cac750206
Eduardo Komaba <ekomaba@proton.me>
2025-01-14 20:07:49 -0300

    Initial implementation of Game class

---
+++ src/Game.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Game.h"
+
+Game::Game() {
+    std::cout << "Game Constructor called!" << std::endl;
+}
+
+Game::~Game() {
+    std::cout << "Game Destructor called!" << std::endl;
+}
+
+void Game::Initialize() {
+
+}
+
+void Game::Run() {
+
+}
+
+void Game::Destroy() {
+
+}
+
+void Game::ProcessInput() {
+
+}
+
+void Game::Update() {
+
+}
+
+void Game::Render() {
+
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,11 @@
-#include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h> 
-#include <glm/glm.hpp>
-#include <imgui/imgui.h>
-#include <sol/sol.hpp>
+#include "Game.h"
 
-int main() {
-    sol::state lua;
-    lua.open_libraries(sol::lib::base);
+int main(int argc, char* argv[]) {
+    Game game;
 
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    std::cout << "Hello World!" << std::endl;
+    game.Initialize();
+    game.Run();
+    game.Destroy();
+    
     return 0;
 }
