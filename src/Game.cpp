83f2db3ec8df46501e09ab4ed07da0200d7b85b6
Eduardo Komaba <ekomaba@proton.me>
2025-01-15 19:30:14 -0300

    Add DeltaTime to ensure smooth movement

--- src/Game.cpp
+++ src/Game.cpp
@@ -100,21 +100,25 @@ glm::vec2 playerVelocity;
 
 void Game::Setup() {
     playerPosition = glm::vec2(10.0, 20.0);
-    playerVelocity = glm::vec2(1.0, 0.0);
+    playerVelocity = glm::vec2(100.0, 5.0);
 }
 
 void Game::Update() {
-    Uint32 currentTicks = SDL_GetTicks();
 
-    Uint32 timeToWait = MILLISECS_PER_FRAME - (currentTicks - millisecsPreviousFrame);
+    int getticks = SDL_GetTicks();
+
+    int timeToWait = MILLISECS_PER_FRAME - (getticks - millisecsPreviousFrame);
     if (timeToWait > 0) {
         SDL_Delay(timeToWait);
     }
 
-    millisecsPreviousFrame = currentTicks;
+    // Difference in ticks since the last frame, converted to seconds
+    double deltaTime = (getticks - millisecsPreviousFrame) / 1000.0;
+
+    millisecsPreviousFrame = getticks;
     
-    playerPosition.x += playerVelocity.x;
-    playerPosition.y += playerVelocity.y;
+    playerPosition.x += playerVelocity.x * deltaTime;
+    playerPosition.y += playerVelocity.y * deltaTime;
 }
 
 void Game::Render() {
