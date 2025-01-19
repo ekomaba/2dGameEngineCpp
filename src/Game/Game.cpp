3f023238e52deaf904bc55da75402e7a9a003f80
Eduardo Komaba <ekomaba@proton.me>
2025-01-19 17:03:45 -0300

    Removing test code

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -101,13 +101,13 @@ void Game::ProcessInput() {
     }
 }
 
-// testing player moving
-glm::vec2 playerPosition;
-glm::vec2 playerVelocity;
-
 void Game::Setup() {
-    playerPosition = glm::vec2(10.0, 20.0);
-    playerVelocity = glm::vec2(100.0, 5.0);
+    // TODO: example...
+    // Entity tank = registry.CreateEntity();
+    // tank.AddComponent<TransformComponent>();
+    // tank.AddComponent<BoxCollider>();
+    // tank.AddComponent<SpriteComponent>("path...");
+    
 }
 
 void Game::Update() {
@@ -126,9 +126,10 @@ void Game::Update() {
     }
 
     millisecsPreviousFrame = getticks;
-    
-    playerPosition.x += playerVelocity.x * deltaTime;
-    playerPosition.y += playerVelocity.y * deltaTime;
+
+    // TODO:
+    // MovementSystem.Update();
+    // AllTheSystems...Update();
 }
 
 void Game::Render() {
@@ -136,26 +137,8 @@ void Game::Render() {
     SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255); 
     SDL_RenderClear(renderer);
 
-    // Loads a PNG texture
-    SDL_Surface *surface = IMG_Load("./assets/images/tank-tiger-right.png");
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-
-    // Free the surface after creating the texture
-    // The surface is only needed as an intermediary to load the image
-    // once the texture is created, the surface is no longer required.
-    SDL_FreeSurface(surface);
-
-    // Destination rectangle that will receive the texture
-    SDL_Rect dstRect = { 
-        static_cast<int>(playerPosition.x), 
-        static_cast<int>(playerPosition.y), 
-        32, 
-        32};
-
-    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
-    SDL_DestroyTexture(texture);
+    // TODO: Render game objects... 
 
     // Presents the renderer (swap the buffers to display the current frame)
     SDL_RenderPresent(renderer);
-
 }
