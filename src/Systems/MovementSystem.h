6afc56a1aff8a4095c983f45f1f1d77f517d7c40
Eduardo Komaba <ekomaba@proton.me>
2025-01-19 17:04:04 -0300

    Adding ECS structure

---
+++ src/Systems/MovementSystem.h
@@ -0,0 +1,26 @@
+/*
+#ifndef MOVEMENTSYSTEM_H
+#define MOVEMENTSYSTEM_H
+
+class MovementSystem : public System {
+
+public:
+    MovementSystem() {
+        // TODO:
+        // RequireComponent<TransformComponent>(); 
+        // RequireComponent<...>();
+    }
+
+    void Update() {
+        // TODO:
+        
+        // for (auto entity : GetEntities()) ... 
+
+        // Update entity position based on its velocity
+        // every frame of the game loop.
+
+    }
+};
+
+#endif
+*/
